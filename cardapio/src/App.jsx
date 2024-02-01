f033e0f7ce53733deddd06ba9256e02357280592
Marcio Tanan <marcio-tanan@hotmail.com>
2024-01-31 21:32:21 -0300

    create menu

--- cardapio/src/App.jsx
+++ cardapio/src/App.jsx
@@ -1,10 +1,16 @@
 import Hashtaurante from './assets/hashtaurante.jpeg'
 import './App.css'
 import { Navegacao } from './Navegacao'
+import { ItemCardapio } from './itemCardapio'
+import { pratosPrincipais, sobremesas, bebidas  } from './cardapio'
 
 export function App(){
   return <>
             <img src={Hashtaurante} alt="" className='capa'/> 
-            <Navegacao/>
+            <Navegacao/>  
+            <div className='menu'>
+              {pratosPrincipais.map(item => <ItemCardapio nome={item.nome} descricao={item.descricao} preco={item.preco} imagem={item.imagem}/>)}
+            </div>   
+                
         </>
 }
